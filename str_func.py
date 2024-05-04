652adf8b9aa8460ae378d4a89aeb6f355f266614
vladimirbutyrskii <vlaver@mail.ru>
2024-05-04 19:57:42 +0300

    3-rd commit - str_func со второй функцией

--- str_func.py
+++ str_func.py
@@ -1,8 +1,18 @@
 def input_str(str_val: str):
     """
-    Вывод текста с заглавными буквами
+    Вывод текста заглавными буквами
     :param str_val:
     :return:
     """
     return str_val.upper()
+
+
+def first_letter_uppercase(str_val: str):
+    """
+    Преобразование первых букв слов в заглавные
+    :param str_val:
+    :return:
+    """
+
+    return str_val.title()
 
